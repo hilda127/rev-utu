7dfc26ce3b1de4218e8c44016c2857f3c3335107
hilda127 <hilda127@users.noreply.github.com>
2022-07-31 19:22:13 +0000

    deploy: c54d9e0a1d0ede539c03319d418938ea342ed9dc

---
+++ component---src-pages-404-js-60f100537639c2f7d9dd.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcreate_react_app=self.webpackChunkcreate_react_app||[]).push([[883],{4393:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var r=n(7294);function a(){return r.createElement("h1",null,"404")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-60f100537639c2f7d9dd.js.map
